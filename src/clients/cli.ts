118408d37a0d9b1b99b8648bd5bdbd3ec4cf018a
Alexander V. Pavlovcik <alexander@pavlovcik.com>
2020-10-23 08:54:30 -0500

    organize cli client

---
+++ src/clients/cli.ts
@@ -0,0 +1,32 @@
+import { main } from "../puppeteer-entry";
+interface IPC {
+	export: TinderProfile[];
+}
+export async function cli() {
+	const arg = process.argv[2];
+
+	if (arg.includes("help")) {
+		console.log(`
+	TINDER SCRAPER
+
+		you can pass in how many matches you want to export.
+		argument is an integer, or you can write "all"
+	`);
+		process.exit(0);
+	} else {
+		let ipc: IPC;
+
+		if (arg == "infinity" || arg == "all") {
+			ipc = await main(Infinity);
+		} else {
+			let amount = parseInt(arg);
+			if (isNaN(amount)) {
+				throw new Error(`couldn't parse argument into amount to scrape`);
+			}
+			ipc = await main(amount);
+		}
+
+		console.log({ ipc });
+		console.log(JSON.stringify(ipc.export, null, "\t"));
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,3 @@
-import { TinderProfile } from "./helpers/@types/tinder";
-import { main } from "./puppeteer-entry";
-interface IPC {
-	export: TinderProfile[];
-}
+import { cli } from "./clients/cli";
 
-const arg = process.argv[2];
-
-if (arg.includes("help")) {
-	console.log(`
-TINDER SCRAPER
-
-    you can pass in how many matches you want to export.
-    argument is an integer, or you can write "all"
-`);
-	process.exit(0);
-} else {
-	(async function cli() {
-		let ipc: IPC;
-
-		if (arg == "infinity" || arg == "all") {
-			ipc = await main(Infinity);
-		} else {
-			let amount = parseInt(arg);
-			if (isNaN(amount)) {
-				throw new Error(`couldn't parse argument into amount to scrape`);
-			}
-			ipc = await main(amount);
-		}
-
-		console.log({ ipc });
-        console.log(JSON.stringify(ipc.export, null, "\t"));
-
-	})();
-}
+cli();
