cb6f3f9edba784f2a24e642bd4cd354f3891c90a
Alexander V. Pavlovcik <alexander@pavlovcik.com>
2020-10-23 08:48:13 -0500

    cli complete

--- src/helpers/process-matches.ts
+++ src/helpers/process-matches.ts
@@ -4,65 +4,73 @@ import { waitAndClick } from "./wait-and-click";
 
 const state = {
 	matchesToProcess: 9, // default, based on Tinder UI chunks
-	storage: [] as any[],
+	storage: [] as TinderProfile[],
 };
 
-export async function processMatches(page: puppeteer.Page, amount?: number) {
-	if (amount) {
-		state.matchesToProcess = amount;
-	}
-
-	// remove "likes-you" match from match per group counter
-	// this makes sure to subtract one from the first group
-	// in case we plan to delete groups (of nine matches per)
-	page
-		.waitForSelector(`a[href="/app/likes-you"]`)
-		.then(() => --state.matchesToProcess)
-		.catch((e) => {});
-
-	// ATTACH PAGE HANDLERS
-	page.on("error", (error) => console.error(error));
-	page.on("console", (message) => console.log(message.text()));
-	page.on("requestfinished", async (request) => {
-		if (request.url().includes("https://api.gotinder.com/user/")) {
-			await userDataReceived(request, page);
+export async function processMatches(page: puppeteer.Page, amount?: number): Promise<TinderProfile[]> {
+	return new Promise(async (resolve, reject) => {
+		if (amount) {
+			state.matchesToProcess = amount;
 		}
-	});
 
-	// START THE CHAIN REACTION
-	await waitAndClick(`a[href*="/app/messages/"]`, page);
+		// remove "likes-you" match from match per group counter
+		// this makes sure to subtract one from the first group
+		// in case we plan to delete groups (of nine matches per)
+		page
+			.waitForSelector(`a[href="/app/likes-you"]`)
+			.then(() => --state.matchesToProcess)
+			.catch((e) => {});
+
+		// ATTACH PAGE HANDLERS
+		page.on("error", (error) => {
+			console.error(error);
+			reject(error); // REJECT
+		});
+		page.on("console", (message) => console.log(message.text()));
+		page.on("requestfinished", async (request) => {
+			if (request.url().includes("https://api.gotinder.com/user/")) {
+				return resolve(await userDataReceived(request, page)); // FIXME RESOLVE HERE OR PASS INSIDE ???
+			}
+		});
+
+		// START THE CHAIN REACTION
+		return await waitAndClick(`a[href*="/app/messages/"]`, page);
+	});
 }
 
-async function userDataReceived(request: puppeteer.Request, page: puppeteer.Page) {
-	// PROMISFY ME
-	console.log(`requestfinished: `, request.url());
-	const profile = await parseFromNetworkRequest(request);
-	state.storage.push(profile);
-	const userID = profile._id;
-	console.log({ state });
+async function userDataReceived(request: puppeteer.Request, page: puppeteer.Page): Promise<TinderProfile[]> {
+	return new Promise(async (resolve, reject) => {
+		console.log(`requestfinished: `, request.url());
+		const profile = await parseFromNetworkRequest(request);
+		state.storage.push(profile);
+		const userID = profile._id;
 
-	if (--state.matchesToProcess > 0) {
-		// theres still more matches in the group
-		await page.evaluate((id) => {
-			const match = document.querySelector(`a[href*="${id}"]`) as any;
-			match.href = "#scraped"; // so it wont get selected in the next line of code
-			const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
-			nextMatch.click();
-		}, userID);
-	} else {
-		// THIS WOULD BE PROMISE RESOLVE
+		// console.log({ state });
 
-		// theres no more matches in the group
-		// console.log(JSON.stringify(database, null, "  "));
-		console.log(state.storage);
-		state.storage = [];
-		// return await page.evaluate((id) => {
-		// const match = document.querySelector(`a[href*="#scraped"]`) as any;
-		// match.parentElement.parentElement.removeChild(match.parentElement); // remove match group!
-		// const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
-		// nextMatch.click();
-		// }, userID);
-	}
+		if (--state.matchesToProcess > 0) {
+			// theres still more matches in the group
+			await page.evaluate((id) => {
+				const match = document.querySelector(`a[href*="${id}"]`) as any;
+				match.href = "#scraped"; // so it wont get selected in the next line of code
+				const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
+				nextMatch.click();
+			}, userID);
+		} else {
+			// THIS WOULD BE PROMISE RESOLVE
+			return resolve(state.storage);
+
+			// theres no more matches in the group
+			// console.log(JSON.stringify(database, null, "  "));
+			// console.log(state.storage);
+			// state.storage = [];
+			// return await page.evaluate((id) => {
+			// const match = document.querySelector(`a[href*="#scraped"]`) as any;
+			// match.parentElement.parentElement.removeChild(match.parentElement); // remove match group!
+			// const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
+			// nextMatch.click();
+			// }, userID);
+		}
+	});
 }
 
 async function parseFromNetworkRequest(request: puppeteer.Request): Promise<TinderProfile> {
--- src/puppeteer-entry.ts
+++ src/puppeteer-entry.ts
@@ -1,29 +1,35 @@
 import pAll from "p-all";
 import puppeteer from "puppeteer";
 import "source-map-support/register";
+import { TinderProfile } from "./helpers/@types/tinder";
 import { browserSetup } from "./helpers/browser-setup";
 import { facebookLogin } from "./helpers/facebook-login";
 import { processMatches } from "./helpers/process-matches";
 import { tinderLogin } from "./helpers/tinder-login";
 import { waitAndClick } from "./helpers/wait-and-click";
-
 process.on("unhandledRejection", console.error);
 
-(async function main() {
+export async function main(howMany?: number) {
 	const page: puppeteer.Page = await browserSetup();
 	await facebookLogin(page);
 	await tinderLogin(page);
 
-	await processMatches(page).catch(async (e) => {
+	const envelope = { export: [] } as { export: TinderProfile[] };
+
+	try {
+		envelope.export = await processMatches(page, howMany);
+	} catch (e) {
+		// async (e) => {
 		// in case its not cached settings, must handle here.
 		await tinderMandetoryPrefs(page);
-		await processMatches(page);
-	});
-})();
+		envelope.export = await processMatches(page, howMany);
+		// };
+	}
+	return envelope;
+}
 
 async function tinderMandetoryPrefs(page: puppeteer.Page) {
 	const ALLOW_LOCATION = `//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]`;
 	const NOTIFICATIONS = `//*[@id="content"]/div/div[2]/div/div/div[2]/button`;
-
 	await pAll([() => waitAndClick(ALLOW_LOCATION, page).catch(() => console.log(`...allow location ok`)), () => waitAndClick(NOTIFICATIONS, page).catch(() => console.log(`...allow notifications ok`))]);
 }
