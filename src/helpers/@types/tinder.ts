8d7d636f4aed2c9cad038a7f7bdc49af50f8596d
Alexander V. Pavlovcik <alexander@pavlovcik.com>
2020-10-25 01:40:17 -0500

    disable loading images and complete full scrape on both views

--- src/helpers/@types/tinder.ts
+++ src/helpers/@types/tinder.ts
@@ -1,26 +1,88 @@
-type NeedsMoreResearch = unknown[] | undefined;
+export interface InstagramProfile {
+	last_fetch_time: string;
+	completed_initial_fetch: boolean;
+	photos: InstagramProfilePhoto[];
+	media_count: number;
+	username: "Tinder";
+}
+ interface InstagramProfilePhoto {
+	image: string;
+	thumbnail: string;
+	ts: string;
+}
+
 export interface TinderProfile {
-	common_friends: NeedsMoreResearch[];
-	common_friend_count: number;
-	distance_mi: number;
-	connection_count: number;
-	common_connections: NeedsMoreResearch[];
-	bio: string;
-	birth_date: string;
-	name: string;
-	jobs: NeedsMoreResearch[];
-	schools: NeedsMoreResearch[];
-	teasers: NeedsMoreResearch[];
-	gender: number;
-	show_gender_on_profile: boolean;
-	birth_date_info: string;
-	ping_time: string;
-	badges: NeedsMoreResearch[];
-	photos: NeedsMoreResearch[];
-	common_likes: unknown[];
-	common_like_count: 0;
-	common_interests: unknown[];
-	s_number: number;
 	_id: string;
+	badges: Badge[];
+	bio: string;
+	birth_date_info: string;
+	birth_date: string;
+	city: { name: string };
+	common_connections: NeedsMoreResearch[];
+	common_friend_count: number;
+	common_friends: NeedsMoreResearch[];
+	common_interests: unknown[];
+	common_like_count: number;
+	common_likes: unknown[];
+	connection_count: number;
+	distance_mi: number;
+	gender: number;
 	is_tinder_u: false;
+	is_travelling: boolean;
+	jobs: Job[];
+	name: string;
+	photos: TinderProfilePhoto[];
+	ping_time: string;
+	s_number: number;
+	schools: NeedsMoreResearch[];
+	show_gender_on_profile: boolean;
+	spotify_theme_track: unknown;
+	teasers: NeedsMoreResearch[];
+	user_interests: { selected_interests: Interest[] } | any;
+	instagram: InstagramProfile;
+}
+
+type NeedsMoreResearch = unknown[] | undefined | any; // FIXME had to add "any" temporarily
+
+interface MatchPhotoProcessedFile {
+	url: string;
+	height: number;
+	width: number;
+}
+
+interface Interest {
+	id: string;
+	name: string;
+}
+
+export interface TinderProfilePhoto {
+	id: string;
+	crop_info: {
+		user: {
+			width_pct: number;
+			x_offset_pct: number;
+			height_pct: number;
+			y_offset_pct: number;
+		};
+		algo: {
+			width_pct: number;
+			x_offset_pct: number;
+			height_pct: number;
+			y_offset_pct: number;
+		};
+		processed_by_bullseye: boolean;
+		user_customized: boolean;
+	};
+	url: string;
+	processedFiles: MatchPhotoProcessedFile[];
+	fileName: string;
+	extension: string;
+}
+interface Job {
+	title: {
+		name: string;
+	};
+}
+interface Badge {
+	type: string;
 }
--- src/helpers/process-matches.ts
+++ src/helpers/process-matches.ts
@@ -1,10 +1,15 @@
 import puppeteer from "puppeteer";
 import { TinderProfile } from "./@types/tinder";
 import { waitAndClick } from "./wait-and-click";
+interface TinderResponse {
+	status: number;
+	results: TinderProfile;
+}
 
 const state = {
 	matchesToProcess: 9, // default, based on Tinder UI chunks
 	storage: [] as TinderProfile[],
+	view: "matches" as "matches" | "messages",
 };
 
 export async function processMatches(page: puppeteer.Page, amount?: number): Promise<TinderProfile[]> {
@@ -26,7 +31,32 @@ export async function processMatches(page: puppeteer.Page, amount?: number): Pro
 			reject(error); // REJECT
 		});
 		page.on("console", (message) => console.log(message.text()));
+
+		await page.setRequestInterception(true);
+		page.on("request", (request) => {
+			if (["image", "stylesheet", "font"].indexOf(request.resourceType()) !== -1) {
+				request.respond({
+					status: 200,
+					// body: "foo"
+				});
+				// request.abort();
+			} else {
+				request.continue();
+			}
+		});
+
 		page.on("requestfinished", async (request) => {
+			if (state.view === "matches") {
+				const element = await page.waitForSelector(`a[href*="/app/messages/"]`);
+
+				const isMessage = await determineIfMessage(element);
+				if (isMessage) {
+					await switchToMessagesView(element, page);
+				}
+
+				// await page.click(`a[href*="/app/messages/"]`); // handled in "resolve"
+			}
+
 			if (request.url().includes("https://api.gotinder.com/user/")) {
 				return resolve(await userDataReceived(request, page));
 			}
@@ -37,46 +67,107 @@ export async function processMatches(page: puppeteer.Page, amount?: number): Pro
 	});
 }
 
-async function userDataReceived(request: puppeteer.Request, page: puppeteer.Page): Promise<TinderProfile[]> {
-	return new Promise(async (resolve, reject) => {
-		console.log(`requestfinished: `, request.url());
-		const profile = await parseFromNetworkRequest(request);
-		state.storage.push(profile);
-		const userID = profile._id;
+async function switchToMessagesView(element: puppeteer.ElementHandle<Element>, page: puppeteer.Page) {
+	await waitAndClick(`//*[@id="content"]/div/div[1]/div/aside/nav/div/div/div/div[1]/div/div[2]`, page); // open "messages" view
+	state.view = "messages";
+	console.log(`switched to "messages" view`);
+}
 
-		if (--state.matchesToProcess > 0) {
-			// theres still more matches in the group
-			await page.evaluate((id) => {
-				const match = document.querySelector(`a[href*="${id}"]`) as any;
-				match.href = "#scraped"; // so it wont get selected in the next line of code
-				const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
-				nextMatch.click();
-			}, userID);
+async function determineIfMessage(element: puppeteer.ElementHandle<Element>) {
+	return await element.evaluate((e) => {
+		if (e.innerHTML.includes(`messageListItem__message`)) {
+			return true;
 		} else {
-			return resolve(state.storage);
-
-			// theres no more matches in the group
-			// console.log(JSON.stringify(database, null, "  "));
-			// console.log(state.storage);
-			// state.storage = [];
-			// return await page.evaluate((id) => {
-			// const match = document.querySelector(`a[href*="#scraped"]`) as any;
-			// match.parentElement.parentElement.removeChild(match.parentElement); // remove match group!
-			// const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
-			// nextMatch.click();
-			// }, userID);
+			return false;
 		}
 	});
 }
 
-async function parseFromNetworkRequest(request: puppeteer.Request): Promise<TinderProfile> {
+async function userDataReceived(request: puppeteer.Request, page: puppeteer.Page): Promise<TinderProfile[]> {
+	return new Promise(async (resolve, reject) => {
+		console.log(`requestfinished: `, request.url(), request.response()?.status());
+
+		const response = await parseFromNetworkRequest(request); // FIXME signal if 403
+
+		let profile;
+		let userID;
+
+		if (response.status !== 200) {
+			// implicit 403 handler
+			console.warn(`REQUEST NOT OK: `, request.url());
+		} else {
+			profile = response.results;
+			state.storage.push(profile); //	save
+			userID = profile._id;
+		}
+
+		await page.evaluate(scrollToBottoms);
+
+		if (--state.matchesToProcess > 0) {
+			// theres still more matches in the group
+			const doneScraping = await continueScraping(page, userID);
+			if (doneScraping) {
+				return resolve(state.storage);
+			}
+		} else {
+			return resolve(state.storage);
+		}
+	});
+}
+
+async function continueScraping(page: puppeteer.Page, userID?: string) {
+	const success = await page.evaluate((id?: string) => {
+		if (id) {
+			const match = document.querySelector(`a[href*="${id}"]`) as any;
+			match.href = "#scraped"; // so it wont get selected in the next line of code
+		}
+		try {
+			const nextMatch = document.querySelector(`a[href*="/app/messages/"]`) as any;
+			nextMatch.click();
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}, userID || false);
+
+	if (!success) {
+		console.log("might be done scraping");
+		console.log("amount requested higher than whats available");
+		console.log(`checking back in "matches" view`);
+
+		await waitAndClick(`#match-tab`, page);
+		await page.evaluate(scrollToBottoms);
+		try {
+			await waitAndClick(`a[href*="/app/messages/"]`, page);
+		} catch (e) {
+			return true;
+		}
+	}
+}
+
+function scrollToBottoms() {
+	try {
+		const matchListNoMessages = document.getElementById(`matchListNoMessages`);
+		// @ts-ignore
+		matchListNoMessages?.scrollTop = matchListNoMessages?.lastChild.getClientRects()[0].top;
+	} catch (e) {
+		console.error(e);
+	}
+	try {
+		const matchListWithMessages = document.getElementById(`matchListWithMessages`);
+		const matchListWithMessagesList = matchListWithMessages?.getElementsByClassName(`messageList`)[0];
+		// @ts-ignore
+		matchListWithMessages?.scrollTop = matchListWithMessagesList?.lastChild.getClientRects()[0].top;
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function parseFromNetworkRequest(request: puppeteer.Request): Promise<TinderResponse> {
 	// const noQueryURL = request.url().split("?").shift() as string;
 	// const userID = noQueryURL.split("/").pop() as string;
-	const { results } = (await request.response()?.json()) as {
-		status: number;
-		results: TinderProfile;
-	};
-	return results;
+	// const { results } =
+	return (await request.response()?.json()) as TinderResponse;
 	// storage.push(USER.results);
 	// return userID;
 }
